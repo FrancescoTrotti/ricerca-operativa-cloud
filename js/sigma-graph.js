ea970d1fb4907836751dd64d4c2ea9dd2fcb0166
FrancescoTrotti <38278130+FrancescoTrotti@users.noreply.github.com>
2020-04-08 08:50:38 +0200

    Add files via upload

---
+++ js/sigma-graph.js
@@ -0,0 +1,57 @@
+var g = $graph_data ;
+
+s = new sigma({graph: g, container: '$container', settings: {
+            minNodeSize: 8,
+            maxNodeSize: 20,
+            minEdgeSize: 5,
+            maxEdgeSize: 6,
+            doubleClickEnabled: false,
+            arrowSizeRatio: 5,
+        }
+});
+
+s.graph.nodes().forEach(function(n) {
+  n.originalColor = n.color;
+});
+s.graph.edges().forEach(function(e) {
+  e.originalColor = e.color;
+});
+
+s.bind('clickNode', function(e) {
+  var nodeId = e.data.node.id,
+      toKeep = s.graph.neighbors(nodeId);
+  toKeep[nodeId] = e.data.node;
+
+  s.graph.nodes().forEach(function(n) {
+    if (toKeep[n.id])
+      n.color = n.originalColor;
+    else
+      n.color = '#eee';
+  });
+
+  s.graph.edges().forEach(function(e) {
+    if (toKeep[e.source] && toKeep[e.target])
+      e.color = e.originalColor;
+    else
+      e.color = '#eee';
+  });
+
+  s.refresh();
+});
+
+s.bind('clickStage', function(e) {
+  s.graph.nodes().forEach(function(n) {
+    n.color = n.originalColor;
+  });
+
+  s.graph.edges().forEach(function(e) {
+    e.color = e.originalColor;
+  });
+
+  s.refresh();
+});
+s.bind('clickNode', function(e) {
+  var kernel = IPython.notebook.kernel;
+    kernel.execute("a.append(5)");
+  });
+
